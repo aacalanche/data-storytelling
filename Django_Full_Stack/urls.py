ff8e7b07b2e9ef96394fcd8ca72559dc8cd283a5
Arturo Calanche <148156489+aacalanche@users.noreply.github.com>
2026-04-22 17:23:05 -0400

    views and url routing created

--- Django_Full_Stack/urls.py
+++ Django_Full_Stack/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from games import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('list/', views.GameListView.as_view(), name='list'),
+    path('form/', views.form, name='form'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('confirm_delete/', views.confirm_delete, name='confirm_delete'),
+    path('games/<int:pk>/', views.GameDetailView.as_view(), name='game_detail'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Game
 
-# Create your views here.
-from django.shortcuts import render
+
 def home(request):
-    context = {"message": "This came from the view."}
-    return render(request, "games/home.html", context)
+    return render(request, "games/home.html")
+
+
+def form(request):
+    return render(request, "games/form.html")
+
+
+def analytics(request):
+    return render(request, "games/analytics.html")
+
+
+def confirm_delete(request):
+    return render(request, "games/confirm_delete.html")
+
+
+class GameListView(ListView):
+    model = Game
+    template_name = "games/list.html"
+    context_object_name = "games"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.path == '/games/complete/':
+            return queryset.filter(is_done=True)
+        return queryset
+    
+
+class GameDetailView(DetailView):
+    model = Game
+    template_name = "games/game_detail.html"
+    context_object_name = "game"
